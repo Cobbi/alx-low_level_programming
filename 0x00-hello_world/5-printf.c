25dc16b9bbee2c4622844aa8a70e186005ab44fd
Bismark Anim <wherescobby@gmail.com>
2023-02-16 03:35:18 -0800

    with proper grammar, but the outcome is a piece of art follwed by new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <std.io>
+/**
+ * main - Entry point
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
