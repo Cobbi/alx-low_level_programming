b4a0b7644febd83eefcc5249730701b335c837d6
Bismark Anim <wherescobby@gmail.com>
2023-02-16 03:27:45 -0800

    Prints exactly Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,11 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle")
-	
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
