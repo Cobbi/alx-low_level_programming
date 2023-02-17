c47c9a0a19329bccd309442560f2d6fd1b079046
Bismark Anim <wherescobby@gmail.com>
2023-02-17 08:50:31 -0800

    Prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+        char alp[26] = "abcdefghijklmnopuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+        return (0);
+}
