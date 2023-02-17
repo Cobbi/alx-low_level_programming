3cc8ff2d052ced070865dd087947b7f715e4db35
Bismark Anim <wherescobby@gmail.com>
2023-02-17 07:25:06 -0800

    Assigns a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - Determines if a number is positive, negative or ze
+ * ro.
  *
  * Returns: Always 0 (Success)
  */
@@ -19,7 +20,7 @@ int main(void)
 	}
     	else if (n == 0)
     	{
-        	printf("%d is  zero\n", n);
+        	printf("%d is zero\n", n);
     	}
     	else
     	{
