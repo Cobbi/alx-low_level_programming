edb63c70daaa8d8993de2c39e1ba84e6992edae6
Bismark Anim <wherescobby@gmail.com>
2023-02-21 05:49:17 -0800

    A function that prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,20 @@
-#ifndef main_h
-	#define main_h
+#include "main.h"
 
 
-	int _putchar(char);
-	void print_alphabet(void);
-	void print_alphabet_x10(void);
-	int _islower(int c);
-	int _isalpha(int c);
-	int print_sign(int n);
-	int _abs(int);
-	int print_last_digit(int);
-	void jack_bauer(void);
-	void times_table(void);
-	int add(int, int);
-	void print_to_98(int n);
-	void print_times_table(int n);
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
 
 
-	#endif
+void print_alphabet(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+
+		_putchar('\n');
+}
 
