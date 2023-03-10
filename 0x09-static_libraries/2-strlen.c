df07603214e29b664c40fbc37273847b4cc4e1d2
Bismark Anim <wherescobby@gmail.com>
2023-03-10 12:11:12 -0800

    Create the static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+
+	return (longi);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This is a README.md file for 0x09-static_libraries
