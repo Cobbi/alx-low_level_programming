4f933aa16da4c4acaa00016311306babf3f81326
Bismark Anim <wherescobby@gmail.com>
2023-03-14 02:13:10 -0700

    Concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,70 +9,35 @@
  */
 
 char *str_concat(char *s1, char *s2)
-
 {
 	char *conct;
-
 	int i, ci;
 
-
-
-
 	if (s1 == NULL)
-
 		s1 = "";
-
 	if (s2 == NULL)
-
 		s2 = "";
-
-
-
 	i = ci = 0;
-
 	while (s1[i] != '\0')
-
 		i++;
-
 	while (s2[ci] != '\0')
-
 		ci++;
-
 	conct = malloc(sizeof(char) * (i + ci + 1));
-
-
-
 	if (conct == NULL)
 
 		return (NULL);
-
 	i = ci = 0;
-
 	while (s1[i] != '\0')
-
 	{
-
 		conct[i] = s1[i];
-
 		i++;
-
 	}
-
-
-
 	while (s2[ci] != '\0')
-
 	{
-
 		conct[i] = s2[ci];
-
 		i++, ci++;
-
 	}
-
 	conct[i] = '\0';
-
 	return (conct);
-
 }
 
