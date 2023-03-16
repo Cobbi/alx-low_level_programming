026711747acf8f6030ab81a1002bac7c720a952d
Bismark Anim <wherescobby@gmail.com>
2023-03-16 08:03:10 -0700

    Creates an array of integers.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -5,6 +5,7 @@
  * *array_range - creates an array of integers
  * @min: minimum range of values stored
  * @max: maximum range of values stored and number of elements
+ *
  * Return: pointer to the new array
  */
 
@@ -14,21 +15,13 @@ int *array_range(int min, int max)
 	int i, size;
 
 	if (min > max)
-
 		return (NULL);
-
 	size = max - min + 1;
-
 	ptr = malloc(sizeof(int) * size);
-
 	if (ptr == NULL)
-
 		return (NULL);
-
 	for (i = 0; min <= max; i++)
-
 		ptr[i] = min++;
-
-	return (ptr)i;
+	return (ptr);
 }
 
